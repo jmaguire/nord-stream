f00ed24b511e95ecb119cf2d0f488a44fbff84c4
jmaguire <jmaguire@users.noreply.github.com>
2023-03-07 13:55:34 -0800

    Update convert_ais.py

--- convert_ais.py
+++ convert_ais.py
@@ -1,3 +1,4 @@
+import os
 import algos as algos
 import json
 import pandas as pd
@@ -16,11 +17,10 @@ SAVE_KML = True
 SAVE_JSON = True
 OUTPUT = "./output/"
 FILETRACKER = "processed.json"
-COLUMNS = ["MMSI", "Latitude", "Longitude", "# Timestamp"]
+COLUMNS = ["MMSI", "Ship type", "Cargo type", "Width",
+           "Length", "Latitude", "Longitude", "# Timestamp"]
 
 # General parallel execution of a function func for data frame df
-
-
 def parallelize_dataframe(df, func, n_cores=CORES):
     df_split = np.array_split(df, n_cores)
     pool = Pool(n_cores)
@@ -31,8 +31,6 @@ def parallelize_dataframe(df, func, n_cores=CORES):
 
 
 # Create point / time data for each ship
-
-
 def pivot_data(df):
     ship_dict = {}
     for index, row in df.iterrows():
@@ -51,24 +49,24 @@ def pivot_data(df):
 
 
 # Take a data frame and extract rows in NS1 or NS2
-
-
 def filter_rows(df):
     points = df.loc[:, "Latitude":"Longitude"]
     df["Inside NS1"] = points.apply(algos.inside_ns1, axis=1)
     df["Inside NS2"] = points.apply(algos.inside_ns2, axis=1)
     return df.loc[(df["Inside NS1"] == 1) | (df["Inside NS2"] == 1)]
 
+# rostock filter!
+def filter_rows_rostock(df):
+    points = df.loc[:, "Latitude":"Longitude"]
+    df["Inside Rostock"] = points.apply(algos.inside_rostock_, axis=1)
+    return df.loc[df["Inside Rostock"] == 1]
 
 # Test for ships that never entered NS1 box but were present just outside of it
-
-
 def filter_rows_sneaky(df):
     points = df.loc[:, "Latitude":"Longitude"]
     df["Inside NS1"] = points.apply(algos.inside_ns1, axis=1)
     df["Inside NS1_large"] = points.apply(algos.inside_ns1_large, axis=1)
     return df.loc[(df["Inside NS1"] == 0) | (df["Inside NS1_large"] == 1)]
-
 
 # Dump KML lines per ship
 def pivot_data_to_kml(ship_dict):
@@ -81,19 +79,20 @@ def pivot_data_to_kml(ship_dict):
         ]
     return kml
 
-
-def process_file(filepath, save_kml=SAVE_KML, save_csv=SAVE_CSV, save_json=SAVE_JSON):
+def process_file(filepath, save_kml=SAVE_KML, save_csv=SAVE_CSV, save_json=SAVE_JSON, filter_func=filter_rows_rostock):
     # Read data as a dataframe. This can be converted to dask
     # for files that are too big for RAM
     start_time = time.time()
     df = pd.read_csv(filepath, usecols=COLUMNS)
     end_time = time.time()
-    print("Read file: ", (end_time - start_time), "seconds", df.shape[0])
+    print("Read file:", (end_time - start_time), "seconds", df.shape[0])
+    print("Rows:", df.shape[0])
 
     # Apply filter function to chunks in parallel
-    # filters to rows in NS1 and NS2
+    # filter rows
     start_time = time.time()
-    filtered_data = parallelize_dataframe(df, filter_rows)
+    # filtered_data = parallelize_dataframe(df, filter_rows)
+    filtered_data = parallelize_dataframe(df, filter_func)
 
     end_time = time.time()
     print(
@@ -120,14 +119,13 @@ def process_file(filepath, save_kml=SAVE_KML, save_csv=SAVE_CSV, save_json=SAVE_
             json.dump(points_data, f)
 
 
-def process_directory(directory, files_processed):
+def process_directory(directory, files_processed, filter_func=filter_rows_rostock):
     for filepath in Path(directory).glob("*.csv"):
         if str(filepath) in files_processed:
             continue
         print("Processing", filepath)
         df = pd.read_csv(filepath)
-        # filtered_data = parallelize_dataframe(df, filter_rows)
-        filtered_data = parallelize_dataframe(df, filter_rows_sneaky)
+        filtered_data = parallelize_dataframe(df, filter_func)
 
         file_stem = OUTPUT + Path(filepath).stem
         filtered_data.to_csv(file_stem + "_filtered.csv", index=False)
@@ -136,7 +134,6 @@ def process_directory(directory, files_processed):
         files_processed.append(str(filepath))
         with open(FILETRACKER, "w") as f:
             json.dump(files_processed, f)
-
 
 def merge_and_process(directory):
     merged_df = helper.merge_files(directory)
@@ -147,8 +144,12 @@ def merge_and_process(directory):
     helper.save_analysis(merged_pivoted, directory + "/filtered_analysis.csv")
     merged_kml.save(directory + "/filtered_data.kml")
 
-
 def main():
+
+    # create output folder
+    if not os.path.exists(OUTPUT):
+        os.makedirs(OUTPUT)
+
     files_processed = []
     if Path(FILETRACKER).is_file():
         with open(FILETRACKER, "r") as f:
@@ -184,7 +185,6 @@ def main():
     else:
         parser.print_usage()
         return sys.exit(1)
-
 
 if __name__ == "__main__":
     main()
